8d73bceea68f359096906e18e9a067bddbd9ae00
Gabriel Bortone <newgabrielbortone@gmail.com>
2020-11-23 18:57:50 -0300

    Implementando Views Importantes

--- Loja Quadrinhos/ViewModels/ProdutoResumoVM.cs
+++ Loja Quadrinhos/ViewModels/ProdutoResumoVM.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Loja_Quadrinhos.ViewModels
+﻿namespace Loja_Quadrinhos.ViewModels
 {
     public class ProdutoResumoVM
     {
