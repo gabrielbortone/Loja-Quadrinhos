bcb2b6541d3ff349ad6da1d0cf6157913dee6a3a
Gabriel Bortone <newgabrielbortone@gmail.com>
2020-11-24 17:10:09 -0300

    Implementado o Component CategoriaMenu

---
+++ Loja Quadrinhos/Components/CategoriaMenu.cs
@@ -0,0 +1,20 @@
+﻿using Loja_Quadrinhos.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loja_Quadrinhos.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoriaMenu(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _unitOfWork.CategoriaRepository.Get();
+            return View(categorias);
+        }
+    }
+}
